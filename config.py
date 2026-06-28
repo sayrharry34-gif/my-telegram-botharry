8bbfb134073ca55c3f35a0ad4398c05b1c4dadc1
sayrharry34-gif <sayrharry34@gmail.com>
2026-06-28 17:15:09 +0700

    Initialize accounts and spam target configuration
    Added account details and spam target chat ID.

---
+++ config.py
@@ -0,0 +1,10 @@
+ACCOUNTS = [
+    {"id": 37047330, "hash": '2264a0263995b06d34ca866134f12441', "name": "session_1"},
+    {"id": 33226528, "hash": 'e11a6e8622de004749b32c4828a9b5b6', "name": "session_2"},
+    {"id": 30636630, "hash": '4bf79480c3ca972b6b0782494b46000d', "name": "session_3"},
+    {"id": 35694835, "hash": '42c233a51bc25c05fe06cc94ca0b5078', "name": "session_4"},
+    {"id": 33935161, "hash": '529e65036cc286f2f435da1cbcbc96fd', "name": "session_5"},
+    {"id": 39865787, "hash": 'cfd2825877820c78babcc94eadddc466', "name": "session_6"},
+    {"id": 34216839, "hash": 'cfa355b3a9026d899cacc3273c6059e2', "name": "session_7"}
+]
+SPAM_TARGET_CHAT = -1003948874074
